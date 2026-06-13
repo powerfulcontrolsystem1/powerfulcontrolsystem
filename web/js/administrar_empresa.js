53ad2fffb4292172ebc837093188dc1aa5f789ae
ivanmundo <45496719+ivanmundo@users.noreply.github.com>
2026-06-13 17:39:14 -0500

    rs: actualizar repositorio y sincronizar VPS

--- web/js/administrar_empresa.js
+++ web/js/administrar_empresa.js
@@ -2052,6 +2052,7 @@ try {
         persistFrameSrc(linkHref, empresaId);
         setActiveByHref(linkHref);
         updateFavoriteButton(linkHref);
+        syncBrowserURLWithFrame(linkHref, empresaId, false);
       });
     });
   }
